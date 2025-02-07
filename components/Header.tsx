0d9cb8bb3fbcd3a423ae9caa08e16b33fa6b33b1
N.I <iworksWeb@gmail.com>
2025-02-08 02:49:51 +0900

    add header style

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,13 @@ const name = "naobe";
 const Header = () => {
   return (
     <header className={styles.container}>
-      <Image src="/icon.jpg" alt="logo" width={100} height={100} />
+      <Image
+        src="/icon.jpg"
+        alt="logo"
+        width={100}
+        height={100}
+        className="rounded-sm"
+      />
       <h1>{name}</h1>
     </header>
   );
