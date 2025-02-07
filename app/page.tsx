e389cf5d158aa950708e5c88af31fe8d7427ea3b
N.I <iworksWeb@gmail.com>
2025-02-08 03:31:13 +0900

    add SSG app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
+import { getPostsData } from "@/lib/post";
+
+interface Post {
+  id: string;
+  title: string;
+  date: string;
+  thumbnail: string;
+}
+
 export const generateMetadata = () => {
   return {
     title: "Home",
@@ -7,7 +16,15 @@ export const generateMetadata = () => {
   };
 };
 
-export default function Home() {
+// SSGの設定を追加
+// App Routerでは、これらのエクスポート設定を使用することで、従来のgetStaticPropsと同様の静的生成（SSG）を実現する。
+export const dynamic = "force-static";
+export const revalidate = false;
+
+export default async function Home(): Promise<React.ReactNode> {
+  const allPostsData = (await getPostsData()) as Post[];
+  console.log(allPostsData);
+
   return (
     <>
       <section className="max-w-3xl mx-auto py-12 px-4">
@@ -23,86 +40,26 @@ export default function Home() {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-            <article className="group relative">
-              <Link
-                href="/posts"
-                className="block transition-colors hover:text-blue-500"
-              >
-                <div className="overflow-hidden rounded-lg">
-                  <Image
-                    src="/opengraph-image.png"
-                    alt="profile"
-                    width={500}
-                    height={100}
-                    className="object-cover transition-transform duration-200 group-hover:scale-105"
-                  />
-                </div>
-                <h2 className="mt-4 text-xl font-semibold">post</h2>
-              </Link>
-              <time className="block mt-2 text-sm text-gray-500">
-                2025-02-07
-              </time>
-            </article>
-            <article className="group relative">
-              <Link
-                href="/posts"
-                className="block transition-colors hover:text-blue-500"
-              >
-                <div className="overflow-hidden rounded-lg">
-                  <Image
-                    src="/opengraph-image.png"
-                    alt="profile"
-                    width={500}
-                    height={100}
-                    className="object-cover transition-transform duration-200 group-hover:scale-105"
-                  />
-                </div>
-                <h2 className="mt-4 text-xl font-semibold">post</h2>
-              </Link>
-              <time className="block mt-2 text-sm text-gray-500">
-                2025-02-07
-              </time>
-            </article>
-            <article className="group relative">
-              <Link
-                href="/posts"
-                className="block transition-colors hover:text-blue-500"
-              >
-                <div className="overflow-hidden rounded-lg">
-                  <Image
-                    src="/opengraph-image.png"
-                    alt="profile"
-                    width={500}
-                    height={100}
-                    className="object-cover transition-transform duration-200 group-hover:scale-105"
-                  />
-                </div>
-                <h2 className="mt-4 text-xl font-semibold">post</h2>
-              </Link>
-              <time className="block mt-2 text-sm text-gray-500">
-                2025-02-07
-              </time>
-            </article>
-            <article className="group relative">
-              <Link
-                href="/posts"
-                className="block transition-colors hover:text-blue-500"
-              >
-                <div className="overflow-hidden rounded-lg">
-                  <Image
-                    src="/opengraph-image.png"
-                    alt="profile"
-                    width={500}
-                    height={100}
-                    className="object-cover transition-transform duration-200 group-hover:scale-105"
-                  />
-                </div>
-                <h2 className="mt-4 text-xl font-semibold">post</h2>
-              </Link>
-              <time className="block mt-2 text-sm text-gray-500">
-                2025-02-07
-              </time>
-            </article>
+            {allPostsData.map(({ id, title, date, thumbnail }) => (
+              <article key={id} className="group relative">
+                <Link
+                  href={`/posts/${id}`}
+                  className="block transition-colors hover:text-blue-500"
+                >
+                  <div className="overflow-hidden rounded-lg">
+                    <Image
+                      src={thumbnail}
+                      alt={title}
+                      width={500}
+                      height={100}
+                      className="object-cover transition-transform duration-200 group-hover:scale-105"
+                    />
+                  </div>
+                  <h2 className="mt-4 text-xl font-semibold">{title}</h2>
+                </Link>
+                <time className="block mt-2 text-sm text-gray-500">{date}</time>
+              </article>
+            ))}
           </div>
         </div>
       </section>
