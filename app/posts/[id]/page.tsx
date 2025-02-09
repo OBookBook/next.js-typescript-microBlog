8f84a068b0cdfe13921a76d87368510d13c6c0c7
N.I <iworksWeb@gmail.com>
2025-02-09 22:24:48 +0900

    add generateStaticParams() app/posts/[id]/page.tsx

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,4 +1,23 @@
 import React from "react";
+import { getAllPostIds } from "@/lib/post";
+
+export const generateMetadata = () => {
+  return {
+    title: "Post Detail",
+    description: "Post Detail page",
+  };
+};
+
+// static generation
+export function generateStaticParams() {
+  const allPostIds = getAllPostIds();
+
+  return allPostIds.map((postId) => ({
+    params: {
+      id: postId.params.id,
+    },
+  }));
+}
 
 const postDetail = () => {
   return <div>postDetail</div>;
