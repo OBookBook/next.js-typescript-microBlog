555927b485599db3c93690b03ba06570e1eb5b70
N.I <iworksWeb@gmail.com>
2025-02-09 22:54:13 +0900

    add Link Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,20 @@
+import Link from "next/link";
 import Image from "next/image";
 import styles from "./Header.module.css";
-
 const name = "naobe";
 
 const Header = () => {
   return (
     <header className={styles.container}>
-      <Image
-        src="/icon.jpg"
-        alt="logo"
-        width={100}
-        height={100}
-        className="rounded-sm"
-      />
+      <Link href="/">
+        <Image
+          src="/icon.jpg"
+          alt="logo"
+          width={100}
+          height={100}
+          className="rounded-sm"
+        />
+      </Link>
       <h1>{name}</h1>
     </header>
   );
